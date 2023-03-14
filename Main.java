61a31aeb954450a9e219951c80d2d681704f4045
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-03-14 05:38:05 +0000

    Add Main Method

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
-class Main {
+
+public class Main {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Tree t1 = new Tree();
+
+    t1.insert("Anusha", 149);
+
+    t1.insert("Kosala", 167);
+
+    t1.insert("Dinusha", 047);
+
+    t1.insert("Mihiri", 066);
+
+    t1.insert("Jayani", 159);
+
+    t1.insert("Nimal", 118);
+
+    t1.insert("Nishantha", 195);
+
+    t1.insert("Avodya", 034);
+
+    t1.insert("Bimali", 105);
+
+    t1.insert("Sampath", 133);
+
   }
+
 }
